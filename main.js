95035215531cbd486d1b243e958570c20e4d2ca7
akshayaO <79805752+akshayaO@users.noreply.github.com>
2022-05-21 15:36:52 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+function preload(){
+}
+
+function setup(){
+    canvas = createCanvas(640,480);
+    canvas.position(110,250);
+}
+
+
+function draw(){
+    
+// Draw a circle at location (30, 30) with a diameter of 20.
+circle(80, 90, 80);
+describe('red circle with black outline ');
+}
+
+function draw(){
+    
+    // Draw a circle at location (30, 30) with a diameter of 20.
+    circle(80, 80, 80);
+    describe('red circle with black outline ');
+    }
+
+
+function take_snapshot(){
+    save('student.png');
+}
+;
